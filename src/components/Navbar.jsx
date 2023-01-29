3dabeb4be66c3dc031942cf2b746d22d3bcd5efb
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-01-28 17:41:13 -0800

    added click event state to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,33 @@ import { MenuItems } from './MenuItems';
 import './NavbarStyles.css';
 
 export class NavbarView extends React.Component {
+	state = { clicked: false };
+
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked })
+	}
+
 	render() {
 		return (
 			<nav className='NavbarItems'>
-				<h1 className='navbar-logo'>Hive Heroes</h1>
+				<h1 className='navbar-logo'>Hive.Heroes</h1>
+
+				<div className='menu-icons' onClick={this.handleClick}>
+					<i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+				</div>
 
 				<ul className='nav-menu'>
 					{MenuItems.map((item, index) => {
 						return (
 							<li key={index}>
-								<Link href="/">
+								<Link className={item.cName} href="/">
 									<i className={item.icon}></i>
 									{item.title}
 								</Link>
 							</li>
 						);
 					})}
+					<button>Sign Up</button>
 
 				</ul>
 
