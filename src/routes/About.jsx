df53f5701f8e213ba88100a8e7560e9ef5998222
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-02-06 17:07:35 -0800

    starting About section

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,10 +1,20 @@
 import { NavbarView } from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutImg from '../assets/bee5.jpg'
 
 function About() {
 	return (
 		<>
 			<NavbarView />
-			<h1>This is About</h1>
+			<Hero
+				cName='hero'
+				heroImg={AboutImg}
+				title='Join the movement and become a HiveHero today!'
+				text='Hiving a sweet-life, one flower at a time.'
+				buttonText='Learn More'
+				url='/'
+				btnClass='show'
+			/>
 		</>
 	)
 }
