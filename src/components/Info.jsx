19e0f545f0e05c7e2bcbf8011575237ea0717811
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-02-11 21:54:47 -0800

    Made hero page reusable & dynamic

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,6 +2,7 @@ import Bumblebee1 from "../assets/bee1.jpg";
 import Bumblebee2 from "../assets/bee3.jpg";
 import Bumblebee3 from "../assets/bee9.jpg";
 import Bumblebee4 from "../assets/bee10.jpg";
+import InfoData from "./InfoData";
 import "./InfoStyles.css";
 
 const Info = () => {
@@ -10,17 +11,7 @@ const Info = () => {
 			<h1>The Power of Bumblebees</h1>
 			<p>"Bee-lieve in the power of pollen."</p>
 
-			<div className="first-info">
-				<div className="des-text">
-					<h2>The Power of Pollen</h2>
-					<p>Bumblebees are essential pollinators that play a vital role in the ecosystem. They are known for their fuzzy, black-and-yellow bodies and their ability to pollinate a wide variety of plants, including many important crops such as tomatoes, blueberries, and apples. However, bumblebee populations have been declining in recent years due to a combination of factors, including habitat loss, pesticide use, and climate change.</p>
-				</div>
-
-				<div className="image">
-					<img alt="img" src={Bumblebee3}></img>
-					<img alt="img" src={Bumblebee4}></img>
-				</div>
-			</div>
+			< InfoData />
 		</div>
 	);
 }
