bbd9f0eed88bc6fbe76a2c285a7d3305d674f7d7
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-02-21 10:31:50 -0800

    Imported Cards & Footer components

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,8 @@
 import Hero from "../components/Hero";
 import Info from "../components/Info";
+import Cards from "../components/Cards";
 import { NavbarView } from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Home() {
 	return (
@@ -16,6 +18,8 @@ function Home() {
 				btnClass='show'
 			/>
 			<Info />
+			<Cards />
+			<Footer />
 		</>
 	)
 }
