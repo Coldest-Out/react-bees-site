ce05c64a5a2c1975274aab766eb40a92c8a15396
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2023-02-21 11:05:56 -0800

    Created AboutUs component w/ styles

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,20 @@
+import "./AboutUsStyles.css";
+
+function AboutUs() {
+	return (
+		<div className="about-container">
+			<h1>History</h1>
+			<p>Urna nunc id cursus metus aliquam eleifend. Augue mauris augue neque gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Tempus urna et pharetra pharetra massa massa. Lectus urna duis convallis convallis tellus id interdum. At erat pellentesque adipiscing commodo elit at imperdiet dui.</p>
+
+			<h1>Vision</h1>
+			<p>Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Pellentesque pulvinar pellentesque habitant morbi. Viverra nibh cras pulvinar mattis nunc sed blandit libero.</p>
+
+			<h1>Accomplishments</h1>
+			<p>Mi eget mauris pharetra et ultrices neque. Quis lectus nulla at volutpat diam. Quam lacus suspendisse faucibus interdum posuere lorem. Quis lectus nulla at volutpat diam ut venenatis. Diam vel quam elementum pulvinar.</p>
+		</div>
+
+
+	);
+}
+
+export default AboutUs;
